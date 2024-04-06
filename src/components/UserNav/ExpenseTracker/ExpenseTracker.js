00cbc919847a61d64045bb1169456934dc20dc0c
Rajath Rao <raorajath1998@gmail.com>
2024-04-06 12:09:41 +0530

    Editing and Deleting expenses

--- src/components/UserNav/ExpenseTracker/ExpenseTracker.js
+++ src/components/UserNav/ExpenseTracker/ExpenseTracker.js
@@ -5,29 +5,32 @@ import Table from "react-bootstrap/Table";
 import "./ExpenseTracker.css";
 import keys from "../../../keys";
 import { useHistory } from "react-router";
+import { Button, ButtonGroup } from "react-bootstrap";
 
 function ExpenseTracker() {
-  const enteredexpense = useRef();
-  const enteredDesc = useRef();
-  const selectedCategory = useRef();
-  const history=useHistory()
+  const history = useHistory();
 
   const [expensesList, setExpnesesList] = useState([]);
-  let email
-  try{
-    email=JSON.parse(localStorage.getItem("token")).email
-    email=email.replace(/[.@]/g, "")
-  }catch(error){
-    console.log(error)
-    alert("login details not found. Please login to add expense...")
-    history.replace("/login")
+  const [isEditMode, setIsEditMode] = useState("");
+
+  const [expense, setExpense] = useState();
+  const [desc, setDesc] = useState();
+  const [category, setCategory] = useState();
+  let email;
+  try {
+    email = JSON.parse(localStorage.getItem("token")).email;
+    email = email.replace(/[.@]/g, "");
+  } catch (error) {
+    console.log(error);
+    alert("login details not found. Please login to add expense...");
+    history.replace("/login");
   }
 
-  const AddExpense = async (e) => {
+  const AddExpense = async (e, edit) => {
     e.preventDefault();
-    let expense = enteredexpense.current.value;
-    let description = enteredDesc.current.value;
-    let category = selectedCategory.current.value;
+    let expense = e.target[0].value;
+    let description = e.target[1].value;
+    let category = e.target[2].value;
     if (!expense) {
       alert("Enter valid expense");
       return;
@@ -45,6 +48,27 @@ function ExpenseTracker() {
       category,
     };
     console.log(temp);
+    if (isEditMode) {
+      try {
+        console.log(
+          `${keys.firebaseUrl}/expenses-list/${email}/${isEditMode}.json`
+        );
+        const res = await axios.patch(
+          `${keys.firebaseUrl}/expenses-list/${email}/${isEditMode}.json`,
+          {
+            expense,
+            description,
+            category,
+          }
+        );
+        console.log("successful edit", res, res.data);
+        getExpense();
+      } catch (error) {
+        console.log("error in edit", error);
+      }
+      setIsEditMode(false);
+      return;
+    }
     try {
       const res = await axios.post(
         `${keys.firebaseUrl}/expenses-list/${email}.json`,
@@ -59,7 +83,9 @@ function ExpenseTracker() {
 
   const getExpense = async () => {
     try {
-      const res = await axios.get(`${keys.firebaseUrl}/expenses-list/${email}.json`);
+      const res = await axios.get(
+        `${keys.firebaseUrl}/expenses-list/${email}.json`
+      );
       console.log("successfully received expenses list", res.data);
       setExpnesesList(
         Object.keys(res.data).map((expenseid) => {
@@ -74,6 +100,47 @@ function ExpenseTracker() {
     getExpense();
   }, []);
 
+  const updateInput = (e, type) => {
+    console.log(e.target.value, type);
+    switch (type) {
+      case "expense":
+        setExpense(e.target.value);
+        break;
+      case "desc":
+        setDesc(e.target.value);
+        break;
+      case "category":
+        setCategory(e.target.value);
+        break;
+    }
+  };
+
+  const handleEdit = (e, edit) => {
+    console.log("edit", edit);
+    setIsEditMode(true);
+    setExpense(edit.expense);
+    setCategory(edit.category);
+    setDesc(edit.description);
+    setIsEditMode(edit.expenseid);
+  };
+
+  const handleDelete = async (expenseid) => {
+    try {
+      const res = await axios.delete(
+        `${keys.firebaseUrl}/expenses-list/${email}/${expenseid}.json`
+      );
+      console.log("successfully deleted",res, res.data);
+      setExpnesesList((prev) => {
+        return [...prev].filter((expense) => {
+          console.log(expense,expense.expenseid,expenseid)
+          return expense.expenseid !== expenseid;
+        });
+      });
+    } catch (error) {
+      console.log("error in deletion", error);
+    }
+  };
+
   return (
     <section className="expense-tracker">
       <form className="form expense-form" onSubmit={AddExpense}>
@@ -82,10 +149,20 @@ function ExpenseTracker() {
           name="money-spent"
           placeholder="Money spent"
           type="number"
-          ref={enteredexpense}
+          value={expense}
+          onChange={(e) => updateInput(e, "expense")}
         />
-        <input name="description" placeholder="Description" ref={enteredDesc} />
-        <select name="categories" ref={selectedCategory}>
+        <input
+          name="description"
+          placeholder="Description"
+          value={desc}
+          onChange={(e) => updateInput(e, "desc")}
+        />
+        <select
+          name="categories"
+          value={category}
+          onChange={(e) => updateInput(e, "category")}
+        >
           <option value="Choose Category">Choose Category</option>
           <option value="Food">Food</option>
           <option value="Transportation">Transportation</option>
@@ -93,7 +170,7 @@ function ExpenseTracker() {
           <option value="Entertainment">Entertainment</option>
         </select>
         <button className="form-btn" type="submit">
-          Add Expense
+          {isEditMode ? "Edit Expense" : "Add Expense"}
         </button>
       </form>
       <section className="expenses-table">
@@ -106,6 +183,7 @@ function ExpenseTracker() {
                 <th>Money Spent</th>
                 <th>Description</th>
                 <th>Category</th>
+                <th>Edit / Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -117,10 +195,47 @@ function ExpenseTracker() {
                     <td>{expense.expense}</td>
                     <td>{expense.description}</td>
                     <td>{expense.category}</td>
+                    <td>
+                      <ButtonGroup>
+                        <Button
+                          onClick={(e) => handleEdit(e, expense)}
+                          style={{ backgroundColor: "transparent" }}
+                          variant="warning"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1.5em"
+                            height="1.5em"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              fill="#ffc107"
+                              d="M21 12a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H5a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1m-15 .76V17a1 1 0 0 0 1 1h4.24a1 1 0 0 0 .71-.29l6.92-6.93L21.71 8a1 1 0 0 0 0-1.42l-4.24-4.29a1 1 0 0 0-1.42 0l-2.82 2.83l-6.94 6.93a1 1 0 0 0-.29.71m10.76-8.35l2.83 2.83l-1.42 1.42l-2.83-2.83ZM8 13.17l5.93-5.93l2.83 2.83L10.83 16H8Z"
+                            ></path>
+                          </svg>
+                        </Button>
+                        <Button
+                          onClick={() => handleDelete(expense.expenseid)}
+                          variant="danger"
+                          style={{ backgroundColor: "transparent" }}
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="1.5em"
+                            height="1.5em"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              fill="red"
+                              d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zm2-4h2V8H9zm4 0h2V8h-2z"
+                            ></path>
+                          </svg>
+                        </Button>
+                      </ButtonGroup>
+                    </td>
                   </tr>
                 );
               })}
-              <tr></tr>
             </tbody>
           </Table>
         ) : (

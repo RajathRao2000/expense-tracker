ed149dfb888e8936123aba475600513f0b12cad5
Rajath Rao <raorajath1998@gmail.com>
2024-04-08 15:36:33 +0530

    subscribing and dispatching actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,32 @@
 import { createStore } from "redux";
 const conuterReducer = (state = { counter: 0 }, action) => {
-  if (action.type == "increment") {
+  if (action.type === "increment") {
     return {
       counter: state.counter + 1,
     };
-  } else if (action.type == "decrement") {
+  } else if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
     };
-  }else if (action.type == "INCREMENTBY2") {
+  } else if (action.type === "INCREMENTBY2") {
     return {
       counter: state.counter + 2,
     };
-  } else if (action.type == "DECREMENTBY2") {
+  } else if (action.type === "DECREMENTBY2") {
     return {
       counter: state.counter - 2,
     };
+  } else if (action.type === "incrementby5") {
+    return {
+      counter: state.counter + 5,
+    };
+  } else if (action.type === "decrementby5") {
+    return {
+      counter: state.counter - 5,
+    };
   }
+
+  return state;
 };
 
 const store = createStore(conuterReducer);
@@ -26,4 +36,4 @@ const counterSubscriber = () => {
   console.log(latestState);
 };
 
-export default store
+export default store;
